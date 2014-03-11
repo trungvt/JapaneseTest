1e9645b5085ed9421e283b970167a03909b973e6
trung-vu <trung-vu@kayac.com>
2014-03-11 20:36:36 +0900

    Edit getSheetByDefault method

--- JapaneseTest/src/com/trungvt/japanese/ExcelReader.java
+++ JapaneseTest/src/com/trungvt/japanese/ExcelReader.java
@@ -1,53 +1,39 @@
 package com.trungvt.japanese;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import jxl.Cell;
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.read.biff.BiffException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelReader {
 	public static HashMap<String, Grammar> readExcelFileForHash(String fileUrl)
 			throws IOException {
 		HashMap<String, Grammar> result = new HashMap<>();
-		Sheet sheet = getSheetByDefault(fileUrl);
-		for (int i = 0; i < sheet.getRows(); i++) {
-			Cell[] cells = sheet.getRow(i);
-			result.put(
-					cells[0].toString(),
-					new Grammar(cells[0].toString(), cells[1].toString(),
-							cells[2].toString(), cells[3].toString(), cells[4]
-									.toString()));
-		}
+		XSSFSheet sheet = getSheetByDefault(fileUrl);
+
 		return result;
 	}
 
 	public static ArrayList<Grammar> readExcelFileForList(String fileUrl)
 			throws IOException {
 		ArrayList<Grammar> result = new ArrayList<Grammar>();
-		Sheet sheet = getSheetByDefault(fileUrl);
-		for (int i = 0; i < sheet.getRows(); i++) {
-			Cell[] cells = sheet.getRow(i);
-			result.add(new Grammar(cells[0].toString(), cells[1].toString(),
-					cells[2].toString(), cells[3].toString(), cells[4]
-							.toString()));
-		}
+		XSSFSheet sheet = getSheetByDefault(fileUrl);
+
 		return result;
 	}
 
-	private static Sheet getSheetByDefault(String fileUrl) throws IOException {
-		Sheet sheet = null;
-		File excelFile = new File(fileUrl);
-		Workbook workbook;
+	private static XSSFSheet getSheetByDefault(String fileUrl) {
+		XSSFSheet sheet = null;
 		try {
-			workbook = Workbook.getWorkbook(excelFile);
-			sheet = workbook.getSheet(0);
-			return sheet;
-		} catch (BiffException e) {
+			File inputFile = new File(fileUrl);
+			FileInputStream fileInputStream = new FileInputStream(inputFile);
+			XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+			sheet = workbook.getSheetAt(0);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return sheet;
